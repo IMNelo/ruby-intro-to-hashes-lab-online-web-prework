d4a76307aa54ac865fad1feb5a7d10ea3c1fcb5d
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-06 06:58:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		monopoly = {railroads: {pieces: "4", names: {}, rent_in_dollars: {one_piece_owned: "25", two_pieces_owned: "50", three_pieces_owned: "100", four_pieces_owned: "200"}}
+		monopoly = {railroads: {pieces: "4", names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}}, rent_in_dollars: {one_piece_owned: "25", two_pieces_owned: "50", three_pieces_owned: "100", four_pieces_owned: "200"}}
     }	
 end
 
@@ -43,7 +43,7 @@ describe "adding key-value pairs to the third level of the monopoly hash that we
         expect(monopoly_with_third_tier.values[0][:names].keys[0]).to eq(:reading_railroad)
         expect(monopoly_with_third_tier.values[0][:names].values[0]).to eq({})
       end
-
+#
       it "sets the 2nd key of the :names hash to a symbol, :pennsylvania_railroad, whose value is an empty hash" do
 
         expect(monopoly_with_third_tier.keys.count).to eq(1)
